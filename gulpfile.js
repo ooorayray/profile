4dbba45685dedf94f3ab9b04d4771d94baa44508
Raymond Lo <raymond.lo@nzxt.com>
2019-09-02 16:01:53 -0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const livereload = require('gulp-livereload');
 
-gulp.task('less', () => {
+const compileLess = () => {
 	return gulp.src(['main.less', 'print.less'])
 		.pipe(less({
 			paths: ['node_modules/']
 		}))
 		.pipe(gulp.dest('public/css/'))
 		.pipe(livereload());
-});
+}
 
 gulp.task('default', () => {
 	livereload.listen();
-	return gulp.watch('*.less', ['less']);
+	return gulp.watch('*.less', compileLess);
 });
